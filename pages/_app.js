69b3e9c2f7e2ade5138e45c42b5037ace0fcad51
Salah Eddine Zaka <zakasalaheddine@gmail.com>
2022-12-27 12:06:20 +0100

    feat: add google analytics scripts

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,13 @@ import {
   QueryClientProvider
 } from '@tanstack/react-query'
 import { useRouter } from 'next/router'
+import Script from 'next/script'
 import { useEffect, useState } from 'react'
 import '../styles/globals.css'
+
+// G-RQ8BJTQY60
+
+const GoogleAnalyticsID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID
 
 function MyApp({ Component, pageProps }) {
   const [queryClient] = useState(() => new QueryClient())
@@ -13,28 +18,42 @@ function MyApp({ Component, pageProps }) {
 
   useEffect(() => {
     const handleRouteChange = (url, { shallow }) => {
-      console.log('checking', typeof window.runZakaLoader)
       if (window.runZakaLoader) {
         window.runZakaLoader()
-        console.log('loader')
+      }
+      if (gtag && gtag.pageview) {
+        gtag.pageview(url)
       }
     }
 
     router.events.on('routeChangeComplete', handleRouteChange)
 
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method:
     return () => {
       router.events.off('routeChangeComplete', handleRouteChange)
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <Component {...pageProps} />
-      </Hydrate>
-    </QueryClientProvider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GoogleAnalyticsID}`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${GoogleAnalyticsID}');
+        `}
+      </Script>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
+      </QueryClientProvider>
+    </>
   )
 }
 
